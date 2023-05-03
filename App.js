689f5b0d924412608a87ef3651558b15a81994af
Huseyn Abishov <92877169+huseynabishov@users.noreply.github.com>
2023-05-04 00:46:22 +0300

    Implemented User Input & Game Logic & Main Screen

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen'; 
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const[userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']}style={styles.rootScreen}>
-    <StartGameScreen />
+      <ImageBackground
+       source={require('./assets/images/background.png')} 
+      resizeMode="cover" 
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}
+      >
+    {screen}
+    </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,15 +1,16 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({children}) {
-
-    function pressHandler() {
-
-    }
+function PrimaryButton({children, onPress}) {
 
 return (
     <View style= {styles.buttonOuterContainer}>
-<Pressable style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer} onPress={pressHandler} android_ripple={{color: '#640233'}}>
-    <Text style={styles.buttonText}>{children}</Text>
+<Pressable 
+style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer} 
+onPress={onPress} 
+android_ripple={{color: '#640233'}}>
+    <Text style={styles.buttonText}>
+        {children}
+        </Text>
 </Pressable>
 </View>
 
