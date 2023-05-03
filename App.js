7bb2463a57be2c29748c762ae39556e56da67a48
Huseyn Abishov <92877169+huseynabishov@users.noreply.github.com>
2023-05-03 23:04:12 +0300

    Implemented Background & Buttons & NumberField & LinearGradient

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen'; 
 
 export default function App() {
   return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']}style={styles.rootScreen}>
     <StartGameScreen />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
+  rootScreen: {
+    flex: 1,
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,8 +6,18 @@ function StartGameScreen() {
 return (
     <View style={styles.inputContainer}>
         <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+
+        <View style={styles.buttonsContainer}>
+            
+            <View style={styles.buttonContainer}>
+            <PrimaryButton>Reset</PrimaryButton>
+            </View>
+
+            <View style={styles.buttonContainer}>
+            <PrimaryButton>Confirm</PrimaryButton>
+            </View>  
+
+        </View>
     </View>
 );
 }
@@ -16,10 +26,12 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create ({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: '#72063c',
+        backgroundColor: '#3b021f',
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black' ,
@@ -37,5 +49,11 @@ const styles = StyleSheet.create ({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
     },
 });
