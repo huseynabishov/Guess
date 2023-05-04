c96bad7140a36a6c9cd68b994709ed79497a9e1f
Huseyn Abishov <92877169+huseynabishov@users.noreply.github.com>
2023-05-04 03:06:44 +0300

    Implemented GameScreen & Adding RandomNumber Logic picker

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,44 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-function GameScreen() {
-    return <Text>Game Screen!</Text>
+import NumberContainer from "../components/game/NumberContainer";
+import Title from "../components/ui/Title";
+
+
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
+
+function GameScreen({userNumber}) {
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const[currentGuess, setCurrentGuess] = useState(initialGuess);
+    
+    return (
+        <View style={styles.screen}> 
+        <Title>Opponent's Guess</Title>
+        <NumberContainer>{currentGuess}</NumberContainer>
+            <View>
+            <Text>Higher or Lower?</Text>
+            {/* { Something } */}
+            </View>
+            {/* <View>LOG ROUNDS</View> */}
+        </View>
+    );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create ({
+    screen: {
+        flex: 1,
+        padding: 24,
+    },
+});
+
